352f8b66a8a7046463b0613171c395c9a4faed3b
Dannon Baker <dannon.baker@gmail.com>
2025-02-07 10:05:43 -0500

    Stub out viz

--- app/views/HomeView/homeView.tsx
+++ app/views/HomeView/homeView.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { SectionAnalytics } from "../../components/Home/components/Section/components/SectionAnalytics/sectionAnalytics";
 import { SectionHelp } from "../../components/Home/components/Section/components/SectionHelp/sectionHelp";
 import { SectionHero } from "../../components/Home/components/Section/components/SectionHero/sectionHero";
+import { SectionViz } from "../../components/Home/components/Section/components/SectionViz/sectionViz";
 import { SectionSubHero } from "../../components/Home/components/Section/components/SectionSubHero/sectionSubHero";
 
 export const HomeView = (): JSX.Element => {
@@ -9,6 +10,7 @@ export const HomeView = (): JSX.Element => {
     <Fragment>
       <SectionHero />
       <SectionSubHero />
+      <SectionViz />
       <SectionAnalytics />
       <SectionHelp />
     </Fragment>
