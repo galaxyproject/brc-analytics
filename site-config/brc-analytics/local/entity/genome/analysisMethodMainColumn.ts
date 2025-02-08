d96656303b2de6ca19eb7009aee744778550ebc1
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-02-07 16:34:46 -0800

    fix: use view model builder for workflows interface (#260)

--- site-config/brc-analytics/local/entity/genome/analysisMethodMainColumn.ts
+++ site-config/brc-analytics/local/entity/genome/analysisMethodMainColumn.ts
@@ -1,12 +1,13 @@
 import { ComponentsConfig } from "@databiosphere/findable-ui/lib/config/entities";
 import * as C from "../../../../../app/components";
+import * as V from "../../../../../app/viewModelBuilders/catalog/brc-analytics-catalog/common/viewModelBuilders";
 
 export const mainColumn: ComponentsConfig = [
   {
     children: [
       {
-        children: [],
         component: C.AnalysisMethodsCatalog,
+        viewBuilder: V.buildGenomeAnalysisMethods,
       },
     ],
     component: C.BackPageContentMainColumn,
