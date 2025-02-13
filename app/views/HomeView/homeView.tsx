7f656f913c183309ef49b22c84eeb445b49d8276
Dannon Baker <dannon.baker@gmail.com>
2025-02-13 13:06:06 -0500

    remove Icicle visualization

--- app/views/HomeView/homeView.tsx
+++ app/views/HomeView/homeView.tsx
@@ -3,7 +3,6 @@ import { SectionAnalytics } from "../../components/Home/components/Section/compo
 import { SectionHelp } from "../../components/Home/components/Section/components/SectionHelp/sectionHelp";
 import { SectionHero } from "../../components/Home/components/Section/components/SectionHero/sectionHero";
 import { SectionViz as Sunburst } from "../../components/Home/components/Section/components/SectionViz/sunburst";
-import { SectionViz as Icicle } from "../../components/Home/components/Section/components/SectionViz/icicle";
 import { SectionSubHero } from "../../components/Home/components/Section/components/SectionSubHero/sectionSubHero";
 
 export const HomeView = (): JSX.Element => {
@@ -12,8 +11,6 @@ export const HomeView = (): JSX.Element => {
       <SectionHero />
       <SectionSubHero />
       <Sunburst />
-      <SectionSubHero />
-      <Icicle />
       <SectionAnalytics />
       <SectionHelp />
     </Fragment>
