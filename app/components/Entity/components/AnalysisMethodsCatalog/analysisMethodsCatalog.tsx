3cb06f9505254b36b7d38df646fab328238c2359
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-02-15 00:10:28 -0800

    feat: show unavailable workflow categories as "coming soon" (#297)

--- app/components/Entity/components/AnalysisMethodsCatalog/analysisMethodsCatalog.tsx
+++ app/components/Entity/components/AnalysisMethodsCatalog/analysisMethodsCatalog.tsx
@@ -3,50 +3,29 @@ import workflows from "../../../../../catalog/output/workflows.json";
 import { AnalysisMethod } from "../AnalysisMethod/analysisMethod";
 import { Props } from "./types";
 import { workflowPloidyMatchesOrganismPloidy } from "../../../../apis/catalog/brc-analytics-catalog/common/utils";
-import { ORGANISM_PLOIDY } from "../../../../apis/catalog/brc-analytics-catalog/common/schema-entities";
-import { WorkflowCategory } from "../../../../apis/catalog/brc-analytics-catalog/common/entities";
 
 export const AnalysisMethodsCatalog = ({
   assemblyPloidy,
   geneModelUrl,
   genomeVersionAssemblyId,
 }: Props): JSX.Element => {
-  const compatibleWorkflows = getCompatibleWorkflows(assemblyPloidy);
   return (
     <AnalysisMethods>
-      {compatibleWorkflows.length === 0
-        ? "None available"
-        : compatibleWorkflows.map((workflowCategory) => {
-            return (
-              <AnalysisMethod
-                key={workflowCategory.category}
-                geneModelUrl={geneModelUrl}
-                genomeVersionAssemblyId={genomeVersionAssemblyId}
-                workflows={workflowCategory.workflows}
-                workflowCategory={workflowCategory}
-              />
-            );
-          })}
+      {workflows.map((workflowCategory) => {
+        const compatibleWorkflows = workflowCategory.workflows.filter(
+          ({ ploidy }) =>
+            workflowPloidyMatchesOrganismPloidy(ploidy, assemblyPloidy)
+        );
+        return (
+          <AnalysisMethod
+            key={workflowCategory.category}
+            geneModelUrl={geneModelUrl}
+            genomeVersionAssemblyId={genomeVersionAssemblyId}
+            workflows={compatibleWorkflows}
+            workflowCategory={workflowCategory}
+          />
+        );
+      })}
     </AnalysisMethods>
   );
 };
-
-function getCompatibleWorkflows(
-  assemblyPloidy: ORGANISM_PLOIDY
-): WorkflowCategory[] {
-  const result: WorkflowCategory[] = [];
-
-  for (const workflowCategory of workflows) {
-    const categoryCompatibleWorkflows = workflowCategory.workflows.filter(
-      ({ ploidy }) =>
-        workflowPloidyMatchesOrganismPloidy(ploidy, assemblyPloidy)
-    );
-    if (categoryCompatibleWorkflows.length > 0)
-      result.push({
-        ...workflowCategory,
-        workflows: categoryCompatibleWorkflows,
-      });
-  }
-
-  return result;
-}
