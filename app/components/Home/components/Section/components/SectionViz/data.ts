4784df29dee5eb2957fd70a8395ff3287f838440
Dannon Baker <dannon.baker@gmail.com>
2025-03-23 23:39:08 -0400

    feat: enhance TreeNode interface with detailed documentation and optional rank property

--- app/components/Home/components/Section/components/SectionViz/data.ts
+++ app/components/Home/components/Section/components/SectionViz/data.ts
@@ -1,8 +1,29 @@
 import data from "catalog/output/ncbi-taxa-tree.json";
 
-interface TreeNode {
+/**
+ * Interface representing a node in the NCBI taxonomy tree
+ */
+export interface TreeNode {
+  /**
+   * Child nodes in the taxonomy tree
+   */
   children: TreeNode[];
+
+  /**
+   * The name of the taxonomic entity
+   */
   name: string;
+
+  /**
+   * The NCBI taxonomy ID as a string
+   */
+  ncbi_tax_id: string;
+
+  /**
+   * Taxonomic rank (e.g., "domain", "phylum", "class", "order", "family", "genus", "species", "strain")
+   * This is optional as some nodes might not have rank specified
+   */
+  rank?: string;
 }
 
 export function getData(): TreeNode {
