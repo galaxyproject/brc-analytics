55b218a693b33e249364e02978382865060c5f18
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-03-31 18:01:07 +1000

    feat: added reference assembly value (#437)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/ReferenceAssemblyStep/referenceAssemblyStep.tsx
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/ReferenceAssemblyStep/referenceAssemblyStep.tsx
@@ -2,16 +2,20 @@ import { Step } from "@databiosphere/findable-ui/lib/components/Stepper/componen
 import { StepContent } from "@databiosphere/findable-ui/lib/components/Stepper/components/Step/components/StepContent/stepContent";
 import { StepLabel } from "@databiosphere/findable-ui/lib/components/Stepper/components/Step/components/StepLabel/stepLabel";
 import { StepProps } from "../types";
+import { Optional } from "@databiosphere/findable-ui/lib/components/Stepper/components/Step/components/StepLabel/components/Optional/optional";
 
 export const ReferenceAssemblyStep = ({
   active,
   completed,
+  genome,
   index,
   label,
 }: StepProps): JSX.Element => {
   return (
     <Step active={active} completed={completed} index={index}>
-      <StepLabel>{label}</StepLabel>
+      <StepLabel optional={<Optional>{genome.accession}</Optional>}>
+        {label}
+      </StepLabel>
       <StepContent>None</StepContent>
     </Step>
   );
