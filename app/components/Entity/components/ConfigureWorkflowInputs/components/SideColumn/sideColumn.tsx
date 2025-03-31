5d9d1117d2325b7c53e7864bfd37d4a94a68eb9e
Fran McDade <frano-m@users.noreply.github.com>
2025-03-31 14:41:30 +1000

    feat: create workflow configuration side column (#431) (#432)
    * feat: introduce workflow configuration page (#422)
    
    * feat: create workflow configuration side column (#431)
    
    * fix: button prop import (#431)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

---
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/SideColumn/sideColumn.tsx
@@ -0,0 +1,27 @@
+import {
+  FluidPaper,
+  GridPaper,
+} from "@databiosphere/findable-ui/lib/components/common/Paper/paper.styles";
+import { CollapsableSection } from "@databiosphere/findable-ui/lib/components/common/Section/components/CollapsableSection/collapsableSection";
+import { KeyValuePairs } from "@databiosphere/findable-ui/lib/components/common/KeyValuePairs/keyValuePairs";
+import { Props } from "./types";
+import {
+  buildGenomeDetails,
+  buildWorkflowDetails,
+} from "../../../../../../viewModelBuilders/catalog/brc-analytics-catalog/common/viewModelBuilders";
+
+export const SideColumn = ({ genome, workflow }: Props): JSX.Element => {
+  return (
+    <FluidPaper>
+      <GridPaper>
+        <CollapsableSection title="Configuration">None</CollapsableSection>
+        <CollapsableSection title="Workflow Details">
+          <KeyValuePairs {...buildWorkflowDetails(workflow)} />
+        </CollapsableSection>
+        <CollapsableSection title="Assembly Details">
+          <KeyValuePairs {...buildGenomeDetails(genome)} />
+        </CollapsableSection>
+      </GridPaper>
+    </FluidPaper>
+  );
+};
