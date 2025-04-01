2e69b6154b273ee3cdc0c93a567ca3dcfb0a8d63
Fran McDade <frano-m@users.noreply.github.com>
2025-04-01 15:37:35 +1000

    feat: update assembly page title and side column for selecting a workflow (#444) (#445)
    * feat: assembly details on side column is rendered before resources (#444)
    
    * feat: updated assembly page top configuration (#444)
    
    * feat: removed fields from assembly details (#444)
    
    * feat: do not display an undefined strain (#444)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- app/components/Entity/components/AnalysisMethods/constants.ts
+++
@@ -1,7 +0,0 @@
-import { TEXT_HEADING } from "@databiosphere/findable-ui/lib/theme/common/typography";
-import { TypographyProps } from "@mui/material";
-
-export const TYPOGRAPHY_PROPS: TypographyProps = {
-  component: "h3",
-  variant: TEXT_HEADING,
-};
