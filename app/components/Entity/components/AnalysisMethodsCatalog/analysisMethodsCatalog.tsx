5dc77fcd8fccd985902c9b41a773e539a45b53c7
Danielle Callan <dmleake92@gmail.com>
2025-04-01 12:13:46 -0400

    feat: filter empty workflow categories by new show_coming_soon field

--- app/components/Entity/components/AnalysisMethodsCatalog/analysisMethodsCatalog.tsx
+++ app/components/Entity/components/AnalysisMethodsCatalog/analysisMethodsCatalog.tsx
@@ -11,12 +11,19 @@ export const AnalysisMethodsCatalog = ({ assembly }: Props): JSX.Element => {
   const {
     query: { entityId },
   } = useRouter();
+
   return (
     <Fragment>
       {workflows.map((workflowCategory) => {
         const compatibleWorkflows = workflowCategory.workflows.filter(
           (workflow) => workflowIsCompatibleWithAssembly(workflow, assembly)
         );
+        if (
+          compatibleWorkflows.length === 0 &&
+          !workflowCategory.showComingSoon
+        ) {
+          return null;
+        }
         return (
           <AnalysisMethod
             entityId={entityId as string}
