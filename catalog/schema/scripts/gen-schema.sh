129eb0ed040e79176bc49615365b4c1cfb2c654f
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-05-26 17:00:52 -0700

    feat: move schema generation logic to package (#540)

--- catalog/schema/scripts/gen-schema.sh
+++ catalog/schema/scripts/gen-schema.sh
@@ -2,14 +2,7 @@
 
 source ./catalog/schema/scripts/source-file-schema-names.sh
 
-# Generate Pydantic models for all source data types
-gen-pydantic ./catalog/py_package/catalog_build/schema/schema.yaml > ./catalog/build/py/generated_schema/schema.py
-
-# Generate TypeScript definitions for all source data types
-python3 ./catalog/schema/scripts/gen-typescript.py ./catalog/py_package/catalog_build/schema/schema.yaml > ./catalog/schema/generated/schema.ts
-
-# Generate a JSON schema for each source file
-for name in ${SOURCE_FILE_SCHEMA_NAMES[@]}
-do
-  gen-json-schema "./catalog/py_package/catalog_build/schema/$name.yaml" > "./catalog/schema/generated/$name.json"
-done
+python3 -m catalog.py_package.catalog_build.schema_utils.gen_schema "${SOURCE_FILE_SCHEMA_NAMES[@]}" \
+  --json-path ./catalog/schema/generated \
+  --py-path ./catalog/build/py/generated_schema --py-name schema \
+  --ts-path ./catalog/schema/generated --ts-name schema
