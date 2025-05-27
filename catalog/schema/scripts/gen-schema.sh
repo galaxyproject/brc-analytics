09e6e30a44b1dccf777fe0703def13d5e38569f2
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-05-26 17:19:02 -0700

    feat: add iwc workflows script to package (#540)

--- catalog/schema/scripts/gen-schema.sh
+++ catalog/schema/scripts/gen-schema.sh
@@ -4,5 +4,5 @@ source ./catalog/schema/scripts/source-file-schema-names.sh
 
 python3 -m catalog.py_package.catalog_build.schema_utils.gen_schema "${SOURCE_FILE_SCHEMA_NAMES[@]}" \
   --json-path ./catalog/schema/generated \
-  --py-path ./catalog/build/py/generated_schema --py-name schema \
+  --py-path ./catalog/py_package/catalog_build/generated_schema --py-name schema \
   --ts-path ./catalog/schema/generated --ts-name schema
