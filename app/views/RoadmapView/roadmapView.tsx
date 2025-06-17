29448f93fa38db32fad0e16038565a9d2516a81c
Danielle Callan <dmleake92@gmail.com>
2025-06-17 12:45:40 -0400

    feat: draft new user facing roadmap

--- app/views/RoadmapView/roadmapView.tsx
+++ app/views/RoadmapView/roadmapView.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { SectionRoadmap } from "../../components/content";
 import { SectionHero } from "../../components/Layout/components/AppLayout/components/Section/components/SectionHero/sectionHero";
 import { BREADCRUMBS } from "./common/constants";
+import { Typography } from "@mui/material";
 
 export const RoadmapView = (): JSX.Element => {
   return (
@@ -9,7 +10,19 @@ export const RoadmapView = (): JSX.Element => {
       <SectionHero
         breadcrumbs={BREADCRUMBS}
         head="Roadmap"
-        subHead="BRC Analytics plans to deliver new powerful analysis functionality while provide access to some features of the VEuPathDB system. This will be an iterative process involving multiple steps in several areas."
+        subHead={
+          <>
+            BRC Analytics aims to seamlessly integrate data from NCBI, analysis
+            tools from Galaxy, and comparative genomics capabilities. We will
+            highlight and provide dedicated resources for priority pathogens
+            while simultaneously supporting neglected organisms. Our goal is to
+            create a{" "}
+            <Typography component="span" sx={{ fontWeight: "bold" }}>
+              community-driven knowledge platform
+            </Typography>{" "}
+            that lowers barriers to entry for complex bioinformatics analyses.
+          </>
+        }
       />
       <SectionRoadmap />
     </Fragment>
