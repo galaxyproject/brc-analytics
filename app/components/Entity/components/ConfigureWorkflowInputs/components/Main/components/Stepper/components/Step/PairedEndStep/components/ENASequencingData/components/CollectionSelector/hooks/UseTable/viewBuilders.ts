9c1d5f7661ce47cfa9ff10177152deb56c156341
Fran McDade <frano-m@users.noreply.github.com>
2025-06-23 19:45:27 -0700

    fix: only one of two paired fastq listed in workflow stepper (#578) (#603)

---
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/PairedEndStep/components/ENASequencingData/components/CollectionSelector/hooks/UseTable/viewBuilders.ts
@@ -0,0 +1,30 @@
+import { CellContext } from "@tanstack/react-table";
+import { ReadRun } from "../../../../types";
+import { LABEL } from "@databiosphere/findable-ui/lib/apis/azul/common/entities";
+
+/**
+ * Returns Fastq FTP cell context.
+ * @param cellContext - Cell context.
+ * @returns Model to be used as cellContext for the BasicCell component.
+ */
+export function buildFastqFTP(
+  cellContext: CellContext<ReadRun, ReadRun["fastq_ftp"]>
+): CellContext<ReadRun, string> {
+  const value = cellContext.getValue();
+
+  // Fastq FTP is a semicolon-separated list of URLs.
+  // Grab the last part of each URL.
+  const values = value
+    .split(";")
+    .map((v) => v.trim().split("/").pop())
+    .filter(Boolean);
+
+  // If no values, return "None".
+  if (values.length === 0)
+    return { getValue: () => LABEL.NONE } as CellContext<ReadRun, string>;
+
+  // Otherwise, return the values as a comma-separated string.
+  return {
+    getValue: () => values.join(", "),
+  } as CellContext<ReadRun, string>;
+}
