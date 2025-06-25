03b40ca61984b39821c55c7a0d1683fccff6a0da
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-06-25 13:59:17 -0700

    feat: sorted priority pathogens (#506)

--- app/views/PriorityPathogenView/priorityPathogensView.tsx
+++ app/views/PriorityPathogenView/priorityPathogensView.tsx
@@ -5,18 +5,25 @@ import { HeroLayout } from "@databiosphere/findable-ui/lib/components/Index/comp
 import { Title } from "@databiosphere/findable-ui/lib/components/common/Title/title";
 import { useConfig } from "@databiosphere/findable-ui/lib/hooks/useConfig";
 import { PriorityPathogens } from "./components/PriorityPathogens/priorityPathogens";
+import { sortPriorityPathogen } from "./utils";
 
 export const PriorityPathogensView = (props: Props): JSX.Element => {
   const { entityConfig } = useConfig();
   const { dimensions } = useLayoutDimensions();
+
+  // Get priority pathogens data.
   const { data } = props;
   const { hits } = data;
+
+  // Sort priority pathogens.
+  const priorityPathogens = hits.sort(sortPriorityPathogen);
+
   return (
     <Index marginTop={dimensions.header.height}>
       <HeroLayout>
         <Title title={entityConfig.explorerTitle} />
       </HeroLayout>
-      <PriorityPathogens priorityPathogens={hits} />
+      <PriorityPathogens priorityPathogens={priorityPathogens} />
     </Index>
   );
 };
---
+++ app/views/PriorityPathogenView/utils.ts
@@ -0,0 +1,12 @@
+import { COLLATOR_CASE_INSENSITIVE } from "@databiosphere/findable-ui/lib/common/constants";
+import { Outbreak } from "../../apis/catalog/brc-analytics-catalog/common/entities";
+
+/**
+ * Sorts priority pathogens by name.
+ * @param pp01 - First priority pathogen.
+ * @param pp02 - Second priority pathogen.
+ * @returns -1 if pp01 < pp02, 0 if pp01 === pp02, 1 if pp01 > pp02.
+ */
+export function sortPriorityPathogen(pp01: Outbreak, pp02: Outbreak): number {
+  return COLLATOR_CASE_INSENSITIVE.compare(pp01.name, pp02.name);
+}
