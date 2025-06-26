9d4135437f86f71cefebc8ab731adcff8de8f87c
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-06-25 20:21:15 -0700

    feat: add responsive view to priority pathogens view (#610)

--- app/views/PriorityPathogensView/components/PriorityPathogens/priorityPathogens.styles.ts
+++ app/views/PriorityPathogensView/components/PriorityPathogens/priorityPathogens.styles.ts
@@ -1,12 +1,17 @@
 import { Grid, Typography } from "@mui/material";
 import styled from "@emotion/styled";
+import { mediaTabletUp } from "@databiosphere/findable-ui/lib/styles/common/mixins/breakpoints";
 
 export const StyledGrid = styled(Grid)`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: 1fr;
 
   .MuiPaper-root {
     align-self: flex-start;
+  }
+
+  ${mediaTabletUp} {
+    grid-template-columns: repeat(2, 1fr);
   }
 `;
 
