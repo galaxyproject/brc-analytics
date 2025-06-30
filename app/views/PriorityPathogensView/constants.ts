642e71a5cff933218559f15582d6115e718cbfce
Fran McDade <frano-m@users.noreply.github.com>
2025-06-30 11:46:15 -0700

    feat: re-order priority to make highest first (#625) (#628)

--- app/views/PriorityPathogensView/constants.ts
+++ app/views/PriorityPathogensView/constants.ts
@@ -1,9 +1,9 @@
 import { OUTBREAK_PRIORITY } from "../../apis/catalog/brc-analytics-catalog/common/schema-entities";
 
 export const PRIORITY: Record<OUTBREAK_PRIORITY, number> = {
-  CRITICAL: 1,
+  CRITICAL: 2,
   HIGH: 3,
-  HIGHEST: 2,
+  HIGHEST: 1,
   MODERATE: 5,
   MODERATE_HIGH: 4,
 };
