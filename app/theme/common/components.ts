eacf76f7195d2b0fbafe74a3f4f41a2067900c0a
Fran McDade <frano-m@users.noreply.github.com>
2025-07-09 04:26:10 +1000

    feat: add priority pathogen (#622)
    * feat: add outbreak tag to assembly list (#509) (#614)
    
    * feat: add outbreak tag to assembly list (#509)
    
    * fix: priority column to priority pathogen name (#509)
    
    * feat: revert priority label for unknown priorities (#509)
    
    * feat: priority tooltip (#509)
    
    * feat: add outbreak tag to organism list (#510) (#615)
    
    * feat: add outbreak tag to assembly list (#509)
    
    * fix: priority column to priority pathogen name (#509)
    
    * feat: add outbreak tag to organism list (#510)
    
    * feat: filter organism and assemblies by priority and priority group (#609) (#616)
    
    * feat: filter organism and assemblies by priority and priority group (#609)
    
    * feat: allow priority column to show on user-select (#609)
    
    * feat: add outbreak tag to workflow (#508) (#618)
    
    * feat: add outbreak tag to workflow (#508)
    
    * feat: updated workflow priority pathogen label (#508)
    
    ---------
    
    * feat: link to organisms and assemblies lists from priority pathogen detail page (#617) (#619)
    
    * feat: link to organisms and assemblies lists from priority pathogen detail page (#617)
    
    * fix: spelling of organisms (#617)
    
    * feat: remove priority pathogen feature flag (#620) (#621)
    
    * feat: order pathogen list by priority then by name (#625) (#627)
    
    * feat: order pathogen list by priority then by name (#625)
    
    * feat: linting (#625)
    
    * feat: re-order priority to make highest first (#625) (#628)

--- app/theme/common/components.ts
+++ app/theme/common/components.ts
@@ -5,6 +5,7 @@ import { PALETTE } from "@databiosphere/findable-ui/lib/styles/common/constants/
 import { BUTTON_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/button";
 import { CHIP_PROPS as APP_CHIP_PROPS } from "../../styles/common/mui/chip";
 import { PALETTE as APP_PALETTE } from "../../styles/common/constants/palette";
+import { CHIP_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/chip";
 
 /**
  * MuiButton Component
@@ -81,6 +82,8 @@ export const MuiChip: Components["MuiChip"] = {
           style: {
             backgroundColor: PALETTE.ALERT_LIGHT,
             color: PALETTE.WARNING_MAIN,
+            // eslint-disable-next-line sort-keys -- disabling key order for readability
+            "&:hover": { backgroundColor: PALETTE.ALERT_LIGHT },
           },
         },
         {
@@ -88,6 +91,29 @@ export const MuiChip: Components["MuiChip"] = {
           style: {
             backgroundColor: APP_PALETTE.CAUTION_LIGHT,
             color: APP_PALETTE.CAUTION_MAIN,
+            // eslint-disable-next-line sort-keys -- disabling key order for readability
+            "&:hover": { backgroundColor: APP_PALETTE.CAUTION_LIGHT },
+          },
+        },
+        {
+          props: { color: CHIP_PROPS.COLOR.DEFAULT },
+          style: {
+            "&:hover": { backgroundColor: PALETTE.SMOKE_MAIN },
+          },
+        },
+        {
+          props: { color: APP_CHIP_PROPS.COLOR.NONE },
+          style: {
+            backgroundColor: "transparent",
+            color: PALETTE.INK_MAIN,
+            // eslint-disable-next-line sort-keys -- disabling key order for readability
+            "&:hover": { backgroundColor: "transparent" },
+          },
+        },
+        {
+          props: { color: CHIP_PROPS.COLOR.WARNING },
+          style: {
+            "&:hover": { backgroundColor: PALETTE.WARNING_LIGHT },
           },
         },
       ],
