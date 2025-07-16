d37f5ae2cb0f4a5ad48ded9e4519c45847939588
mvdbeek <m.vandenbeek@gmail.com>
2025-07-16 16:00:47 +0200

    Skip GTF configuration if we don't need it

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/stepper.tsx
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/stepper.tsx
@@ -2,10 +2,15 @@ import { STEPPER_PROPS, STEPS } from "./constants";
 import { StyledStepper } from "./stepper.styles";
 import { Props } from "./types";
 import { useStepper } from "./hooks/UseStepper/hook";
-
-const steps = STEPS;
+import { WORKFLOW_PARAMETER_VARIABLE } from "../../../../../../../../apis/catalog/brc-analytics-catalog/common/schema-entities";
 
 export const Stepper = ({ workflow, ...props }: Props): JSX.Element => {
+  const requiresGTF = workflow.parameters.some(
+    (param) => param.variable === WORKFLOW_PARAMETER_VARIABLE.GENE_MODEL_URL
+  );
+  const steps = requiresGTF
+    ? STEPS
+    : STEPS.filter((step) => step.key !== "geneModelUrl");
   const { activeStep, onContinue, onEdit } = useStepper(steps);
   return (
     <StyledStepper activeStep={activeStep} {...STEPPER_PROPS}>
