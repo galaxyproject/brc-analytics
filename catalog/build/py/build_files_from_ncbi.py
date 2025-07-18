39076ba2687929981814847c00829415bf8b8f14
Danielle Callan <dmleake92@gmail.com>
2025-07-18 13:10:40 -0400

    feat: add helminth assemblies, some new bacteria

--- catalog/build/py/build_files_from_ncbi.py
+++ catalog/build/py/build_files_from_ncbi.py
@@ -29,6 +29,9 @@ TAXONOMIC_GROUPS_BY_TAXONOMY_ID = {
     5204: "Basidiomycota",
     4761: "Chytridiomycota",
     1913637: "Mucoromycota",
+    6157: "Platyhelminthes",
+    6231: "Nematoda",
+    2157: "Archaea"
 }
 
 # TAXONOMIC_LEVELS_FOR_TREE should be listed in order, as the order is used in building the taxonomy tree;
