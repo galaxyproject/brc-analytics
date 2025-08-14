5fcb6d5352beba9904d883dc7dd138abf6ddac02
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-08-14 16:31:41 +1000

    refactor: column definitions into own file (#700)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSelector/hooks/UseTable/constants.ts
+++
@@ -1,6 +0,0 @@
-import { ColumnMeta } from "@tanstack/react-table";
-import { ReadRun } from "../../../../types";
-
-export const META: ColumnMeta<ReadRun, unknown> = {
-  width: { max: "1.2fr", min: "120px" },
-};
