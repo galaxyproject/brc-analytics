5d02f72715a485cfc9df00113cf5fb31a62ef891
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-08-14 16:06:37 +1000

    wip

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSelector/components/Filters/constants.ts
+++
@@ -1,6 +0,0 @@
-import { MenuProps } from "@mui/material";
-
-export const MENU_PROPS: Omit<MenuProps, "anchorEl" | "onClose" | "open"> = {
-  anchorOrigin: { horizontal: "left", vertical: "bottom" },
-  transformOrigin: { horizontal: "left", vertical: "top" },
-};
