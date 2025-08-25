994be5b74d214891885a7351b5778a76efa1ca92
Fran McDade <frano-m@users.noreply.github.com>
2025-08-26 02:15:37 +1000

    feat: update "upload my data" ui (#741) (#743)
    * feat: update "upload my data" ui (#741)
    
    * feat: updated title text (#741)
    
    ---------
    
    Co-authored-by: Fran McDade <18710366+frano-m@users.noreply.github.com>

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/UploadMyData/uploadMyData.styles.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/UploadMyData/uploadMyData.styles.ts
@@ -1,13 +1,7 @@
-import { PALETTE } from "@databiosphere/findable-ui/lib/styles/common/constants/palette";
 import styled from "@emotion/styled";
-import { Paper } from "@mui/material";
+import { Alert } from "@mui/material";
 
-export const StyledPaper = styled(Paper)`
-  background-color: ${PALETTE.SMOKE_LIGHT};
-  display: grid;
-  gap: 8px;
-  justify-items: center;
-  padding: 24px;
-  text-align: center;
+export const StyledAlert = styled(Alert)`
+  padding: 20px;
   width: 100%;
 `;
