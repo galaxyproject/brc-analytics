d167b084655e647ec86a3265478e30283d835a6e
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-08-27 09:38:08 +1000

    refactor: category constant (#751)

--- site-config/ga2/local/index/common/category/configs.ts
+++ site-config/ga2/local/index/common/category/configs.ts
@@ -1,19 +1,16 @@
 import { CategoryConfig } from "@databiosphere/findable-ui/lib/common/categories/config/types";
-import {
-  BRC_DATA_CATALOG_CATEGORY_KEY,
-  BRC_DATA_CATALOG_CATEGORY_LABEL,
-} from "../../../../category";
+import { GA2_CATEGORY_KEY, GA2_CATEGORY_LABEL } from "../../../../category";
 import { mapSelectCategoryValue } from "./mapSelectCategoryValue";
 import { mapPriority, mapPriorityPathogenName } from "./utils";
 
 export const PRIORITY: CategoryConfig = {
-  key: BRC_DATA_CATALOG_CATEGORY_KEY.PRIORITY,
-  label: BRC_DATA_CATALOG_CATEGORY_LABEL.PRIORITY,
+  key: GA2_CATEGORY_KEY.PRIORITY,
+  label: GA2_CATEGORY_LABEL.PRIORITY,
   mapSelectCategoryValue: mapSelectCategoryValue(mapPriority),
 };
 
 export const PRIORITY_PATHOGEN_NAME: CategoryConfig = {
-  key: BRC_DATA_CATALOG_CATEGORY_KEY.PRIORITY_PATHOGEN_NAME,
-  label: BRC_DATA_CATALOG_CATEGORY_LABEL.PRIORITY_PATHOGEN_NAME,
+  key: GA2_CATEGORY_KEY.PRIORITY_PATHOGEN_NAME,
+  label: GA2_CATEGORY_LABEL.PRIORITY_PATHOGEN_NAME,
   mapSelectCategoryValue: mapSelectCategoryValue(mapPriorityPathogenName),
 };
--- site-config/ga2/local/index/common/column/columnDefs.ts
+++ site-config/ga2/local/index/common/column/columnDefs.ts
@@ -3,10 +3,7 @@ import {
   BRCDataCatalogGenome,
   BRCDataCatalogOrganism,
 } from "../../../../../../app/apis/catalog/brc-analytics-catalog/common/entities";
-import {
-  BRC_DATA_CATALOG_CATEGORY_KEY,
-  BRC_DATA_CATALOG_CATEGORY_LABEL,
-} from "../../../../category";
+import { GA2_CATEGORY_KEY, GA2_CATEGORY_LABEL } from "../../../../category";
 import * as C from "../../../../../../app/components";
 import * as V from "../../../../../../app/viewModelBuilders/catalog/brc-analytics-catalog/common/viewModelBuilders";
 
@@ -15,7 +12,7 @@ export const PRIORITY: ColumnConfig<
 > = {
   componentConfig: { component: C.BasicCell, viewBuilder: V.buildPriority },
   enableHiding: true,
-  header: BRC_DATA_CATALOG_CATEGORY_LABEL.PRIORITY,
-  id: BRC_DATA_CATALOG_CATEGORY_KEY.PRIORITY,
+  header: GA2_CATEGORY_LABEL.PRIORITY,
+  id: GA2_CATEGORY_KEY.PRIORITY,
   width: "auto",
 };
