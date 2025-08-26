d99ec0af8e0bb8091116b028583bf6c6c15a04b7
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-08-27 09:40:58 +1000

    feat: updated ga2 env (#751)

--- site-config/ga2/dev/config.ts
+++ site-config/ga2/dev/config.ts
@@ -1,7 +1,7 @@
 import { SiteConfig } from "@databiosphere/findable-ui/lib/config/entities";
 import { makeConfig } from "../local/config";
 
-const BROWSER_URL = "https://brc-analytics.dev.clevercanary.com";
+const BROWSER_URL = "https://ga2.dev.clevercanary.com";
 
 const config: SiteConfig = makeConfig(BROWSER_URL);
 
