12994bb65ea95a7c90d5dca9222908ef2823a257
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-08-27 09:30:43 +1000

    feat: added ga2 entities (#751)

---
+++ app/apis/catalog/ga2/entities.ts
@@ -0,0 +1,47 @@
+export type GA2Entity = GA2AssemblyEntity | GA2OrganismEntity;
+
+/**
+ * TODO: Confirm the shape of Assembly and Organism entities, including sra_data.
+ */
+
+export interface GA2AssemblyEntity {
+  accession: string;
+  annotationStatus: string | null;
+  chromosomes: number | null;
+  coverage: string | null;
+  gcPercent: number | null;
+  geneModelUrl: string | null;
+  isRef: "No" | "Yes";
+  length: number;
+  level: string;
+  ncbiTaxonomyId: string;
+  scaffoldCount: number | null;
+  scaffoldL50: number | null;
+  scaffoldN50: number | null;
+  species: string;
+  speciesTaxonomyId: string;
+  sra_data: SRAData;
+  strain: string | null;
+  taxonomicGroup: string[];
+  tolId: string;
+  ucscBrowserUrl: string | null;
+}
+
+export interface GA2OrganismEntity {
+  assemblyCount: number;
+  assemblyTaxonomyIds: string[];
+  genomes: GA2AssemblyEntity[];
+  maxScaffoldN50: number;
+  ncbiTaxonomyId: string;
+  species: string;
+  taxonomicGroup: string[];
+  tolId: string;
+}
+
+/**
+ * TODO: Confirm the shape of SRAData.
+ */
+
+export interface SRAData {
+  accession: string;
+}
