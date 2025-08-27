c0306539be19c366b0146e884a8d3cacd8888288
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-08-27 11:41:04 +1000

    refactor: genome config categories / columns (#751)

---
+++ site-config/ga2/local/index/genome/columns.ts
@@ -0,0 +1,39 @@
+import { GA2AssemblyEntity } from "../../../../../app/apis/catalog/ga2/entities";
+import { ColumnConfig } from "@databiosphere/findable-ui/lib/config/entities";
+import {
+  ANALYZE_GENOME,
+  ACCESSION,
+  SPECIES,
+  STRAIN,
+  TAXONOMY_ID,
+  TAXONOMIC_GROUP,
+  IS_REF,
+  LEVEL,
+  CHROMOSOMES,
+  LENGTH,
+  SCAFFOLD_COUNT,
+  SCAFFOLD_N50,
+  SCAFFOLD_L50,
+  COVERAGE,
+  GC_PERCENT,
+  ANNOTATION_STATUS,
+} from "./columnDefs";
+
+export const COLUMNS: ColumnConfig<GA2AssemblyEntity>[] = [
+  ANALYZE_GENOME,
+  ACCESSION,
+  SPECIES,
+  STRAIN,
+  TAXONOMY_ID,
+  TAXONOMIC_GROUP,
+  IS_REF,
+  LEVEL,
+  CHROMOSOMES,
+  LENGTH,
+  SCAFFOLD_COUNT,
+  SCAFFOLD_N50,
+  SCAFFOLD_L50,
+  COVERAGE,
+  GC_PERCENT,
+  ANNOTATION_STATUS,
+];
