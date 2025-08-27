a6fc0c66b9ce1f3cd333199f7a5536721a7f2b2e
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-08-27 15:00:04 -0700

    refactor: factor out most calculations from assemblies and organisms build (#752)

--- catalog/build/ts/utils.ts
+++ catalog/build/ts/utils.ts
@@ -3,6 +3,79 @@ import fsp from "fs/promises";
 import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import YAML from "yaml";
+import {
+  OrganismPloidy,
+  Organism as SourceOrganism,
+  Organisms as SourceOrganisms,
+} from "../../schema/generated/schema";
+import { Outbreak } from "../../../app/apis/catalog/brc-analytics-catalog/common/entities";
+
+export async function getSourceOrganismsByTaxonomyId(
+  sourceOrganismsPath: string
+): Promise<Map<string, SourceOrganism>> {
+  const sourceOrganisms =
+    await readYamlFile<SourceOrganisms>(sourceOrganismsPath);
+  return new Map(
+    sourceOrganisms.organisms.map((sourceOrganism) => [
+      String(sourceOrganism.taxonomy_id),
+      sourceOrganism,
+    ])
+  );
+}
+
+/**
+ * Get the ploidy for an assembly, logging a message if the assembly will be skipped.
+ * @param sourceOrganismsByTaxonomyId - Source organisms mapped by species taxonomy ID, to get ploidy from.
+ * @param speciesTaxonomyId - Species taxonomy ID of the assembly.
+ * @param willSkipIfNull - Whether the assembly will be skipped if the returned ploidy is null.
+ * @param assemblyAccession - Assembly accession to reference in the log message.
+ * @returns array of ploidy values, or null if organism info was not found.
+ */
+export function getPloidyForAssembly(
+  sourceOrganismsByTaxonomyId: Map<string, SourceOrganism>,
+  speciesTaxonomyId: string,
+  willSkipIfNull: boolean = false,
+  assemblyAccession?: string
+): OrganismPloidy[] | null {
+  const ploidy =
+    sourceOrganismsByTaxonomyId.get(speciesTaxonomyId)?.ploidy ?? null;
+  if (willSkipIfNull && ploidy === null) {
+    console.log(
+      `Skipping assembly${assemblyAccession ? " " + assemblyAccession : ""} [tax_id: ${speciesTaxonomyId}] - ploidy not found`
+    );
+  }
+  return ploidy;
+}
+
+export function getSpeciesStrainName(
+  sourceTaxonomicLevelSpecies: string,
+  sourceTaxonomicLevelStrain: string,
+  sourceStrain: string
+): string {
+  return (
+    sourceTaxonomicLevelStrain ||
+    (sourceStrain
+      ? `${sourceTaxonomicLevelSpecies} strain ${sourceStrain}`
+      : "None")
+  );
+}
+
+/**
+ * Get the outbreak associated with the first of the given lineage taxa that has an assocated outbreak, or null if none is found.
+ * @param outbreaksByTaxonomyId - Map from taxonomy ID (number) to outbreak.
+ * @param lineageTaxonomyIds - Taxonomic lineage (array of taxonomy ID strings).
+ * @returns matching outbreak, or null.
+ */
+export function getOutbreakMatchingLineage(
+  outbreaksByTaxonomyId: Map<number, Outbreak>,
+  lineageTaxonomyIds: string[]
+): Outbreak | null {
+  for (const stringId of lineageTaxonomyIds) {
+    const outbreak = outbreaksByTaxonomyId.get(Number(stringId));
+    if (outbreak !== undefined) return outbreak;
+  }
+  return null;
+}
 
 export async function readValuesFile<T>(
   filePath: string,
@@ -66,6 +139,19 @@ export function verifyUniqueIds<T>(
   }
 }
 
+export function incrementValue(value?: number): number {
+  return (value ?? 0) + 1;
+}
+
+export function accumulateArrayOrNullValues<T>(
+  array: T[] | null | undefined,
+  values: T[] | null
+): T[] | null {
+  return values
+    ? accumulateArrayValue(array ?? undefined, ...values)
+    : (array ?? null);
+}
+
 export function accumulateArrayValue<T>(
   array: T[] | undefined,
   ...values: T[]
@@ -82,6 +168,10 @@ export function accumulateArrayValue<T>(
 
 export function defaultStringToNone(value: string): string {
   return value || "None";
+}
+
+export function parseListOrNull(value: string): string[] | null {
+  return value ? value.split(",") : null;
 }
 
 export function parseList(value: string): string[] {
