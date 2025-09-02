e1ff858432ac6c99dc9d2dca3eeacf23a0bbb7d2
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-09-02 10:11:04 +1000

    feat: remove redirect on home page (#751)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,13 @@ import { GetStaticProps } from "next";
 import { StyledMain } from "../app/components/Layout/components/Main/main.styles";
 import { HomeView } from "../app/views/HomeView/homeView";
 import { config } from "../app/config/config";
-import { ROUTES } from "../routes/constants";
 
 export const Home = (): JSX.Element => {
   return <HomeView />;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { allowedPaths, appTitle } = config();
-
-  if (allowedPaths && !allowedPaths.includes("/")) {
-    return {
-      redirect: {
-        destination: ROUTES.ORGANISMS,
-        permanent: false,
-      },
-    };
-  }
+  const { appTitle } = config();
 
   return {
     props: {
