9f944cab338e94ee8df7140b995379f5a88315c4
Patrik Smeds <patrik.smeds@gmail.com>
2025-09-08 17:09:57 -0400

    feat: add a roadmap for GA2

--- pages/roadmap/index.tsx
+++ pages/roadmap/index.tsx
@@ -1,10 +1,17 @@
 import { GetStaticProps } from "next";
 import { StyledPagesMain } from "../../app/components/Layout/components/Main/main.styles";
 import { RoadmapView } from "../../app/views/RoadmapView/roadmapView";
+import { RoadmapViewGA2 } from "../../app/views/RoadmapView/roadmapViewGA2";
 import { config } from "../../app/config/config";
+import { APP_KEYS } from "../../site-config/common/constants";
 import { ROUTES } from "../../routes/constants";
 
 export const Roadmap = (): JSX.Element => {
+  const { appKey } = config();
+
+  if (appKey === APP_KEYS.GA2) {
+    return <RoadmapViewGA2 />;
+  }
   return <RoadmapView />;
 };
 
