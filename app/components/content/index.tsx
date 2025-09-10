3bba9cabb17527e5dfe57c85843a2e41fb5e3983
Patrik Smeds <patrik.smeds@gmail.com>
2025-09-10 11:39:44 -0400

    feat: GA2 about page (#753)

--- app/components/content/index.tsx
+++ app/components/content/index.tsx
@@ -1,2 +1,3 @@
 export { default as SectionAbout } from "./sectionAbout.mdx";
+export { default as SectionAboutGA2 } from "./sectionAboutGA2.mdx";
 export { default as SectionRoadmap } from "./sectionRoadmap.mdx";
