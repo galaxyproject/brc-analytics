a5c907ff672de9b4eb99607ea2c2333aa21a30e1
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-09-19 16:56:41 +1000

    feat: removed toolbar (#852)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSummary/collectionSummary.styles.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSummary/collectionSummary.styles.ts
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
 import { RoundedPaper } from "@databiosphere/findable-ui/lib/components/common/Paper/components/RoundedPaper/roundedPaper";
-import { PALETTE } from "@databiosphere/findable-ui/lib/styles/common/constants/palette";
 
 export const StyledRoundedPaper = styled(RoundedPaper)`
-  background-color: ${PALETTE.SMOKE_MAIN};
-  display: grid;
-  gap: 1px;
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  justify-content: space-between;
+  padding: 16px;
   width: 100%;
 `;
--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSummary/collectionSummary.tsx
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSummary/collectionSummary.tsx
@@ -3,7 +3,6 @@ import { StyledRoundedPaper } from "./collectionSummary.styles";
 import { Props } from "./types";
 import { TYPOGRAPHY_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/typography";
 import { BUTTON_PROPS } from "@databiosphere/findable-ui/lib/components/common/Button/constants";
-import { StyledToolbar } from "@databiosphere/findable-ui/lib/components/Table/components/TableToolbar/tableToolbar.styles";
 
 export const CollectionSummary = ({
   onClear,
@@ -15,19 +14,17 @@ export const CollectionSummary = ({
   if (selectedCount === 0) return null;
   return (
     <StyledRoundedPaper elevation={0}>
-      <StyledToolbar>
-        <Typography variant={TYPOGRAPHY_PROPS.VARIANT.BODY_400}>
-          {count} Collection{count > 1 ? "s" : ""} Selected
-        </Typography>
-        <Grid container gap={2}>
-          <Button {...BUTTON_PROPS.SECONDARY_CONTAINED} onClick={onEdit}>
-            Edit list
-          </Button>
-          <Button {...BUTTON_PROPS.SECONDARY_CONTAINED} onClick={onClear}>
-            Clear all
-          </Button>
-        </Grid>
-      </StyledToolbar>
+      <Typography variant={TYPOGRAPHY_PROPS.VARIANT.BODY_400}>
+        {count} Collection{count > 1 ? "s" : ""} Selected
+      </Typography>
+      <Grid container gap={2}>
+        <Button {...BUTTON_PROPS.SECONDARY_CONTAINED} onClick={onEdit}>
+          Edit list
+        </Button>
+        <Button {...BUTTON_PROPS.SECONDARY_CONTAINED} onClick={onClear}>
+          Clear all
+        </Button>
+      </Grid>
     </StyledRoundedPaper>
   );
 };
