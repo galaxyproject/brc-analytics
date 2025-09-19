9e2b00e35f76d9964a5dfd04201f5bd21052d6d4
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-09-19 16:27:34 +1000

    feat: linting (#767)

--- app/components/Home/components/Section/components/ga2/SectionHero/components/Carousel/cards/constants.ts
+++ app/components/Home/components/Section/components/ga2/SectionHero/components/Carousel/cards/constants.ts
@@ -2,15 +2,9 @@ import { CardProps } from "@databiosphere/findable-ui/lib/components/common/Card
 import * as MDX from "../content";
 
 export const CAROUSEL_CARDS: Pick<CardProps, "text">[] = [
-  // {
-  //   text: MDX.Webinars({}),
-  // },
   {
     text: MDX.Roadmap({}),
   },
-  // {
-  //   text: MDX.Webinar20250326({}),
-  // },
   {
     text: MDX.ShareUsageAndJoinAdvisoryPanel({}),
   },
