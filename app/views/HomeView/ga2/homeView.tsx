3a7d67afd079691638f60d214f2d6a5f47c42899
Patrik Smeds <patrik.smeds@gmail.com>
2025-09-23 08:55:26 -0400

    feat: add organism selector to ga2 landing page

--- app/views/HomeView/ga2/homeView.tsx
+++ app/views/HomeView/ga2/homeView.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { SectionHero } from "../../../components/Home/components/Section/components/ga2/SectionHero/sectionHero";
+import { SectionAssemblies } from "../../../components/Home/components/Section/components/ga2/SectionAssemblies/sectionAssemblies";
 import { SectionSubHero } from "../../../components/Home/components/Section/components/ga2/SectionSubHero/sectionSubHero";
 import { SectionAnalyticsAndData } from "../../../components/Home/components/Section/components/ga2/SectionAnalyticsAndData/sectionAnalyticsAndData";
 import { SectionHelp } from "../../../components/Home/components/Section/components/ga2/SectionHelp/sectionHelp";
@@ -9,6 +10,7 @@ export const HomeView = (): JSX.Element => {
     <Fragment>
       <SectionHero />
       <SectionSubHero />
+      <SectionAssemblies />
       <SectionAnalyticsAndData />
       <SectionHelp />
     </Fragment>
