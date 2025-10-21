a8a9a66bce2d7ac41844a8f7f510fff8495ad197
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-21 21:41:25 +1000

    feat: added section styles (#331)

--- app/views/LearnContentView/learnContentView.tsx
+++ app/views/LearnContentView/learnContentView.tsx
@@ -3,6 +3,11 @@ import { StaticProps } from "../../docs/common/staticGeneration/types";
 import { SectionHero } from "../../components/Layout/components/AppLayout/components/Section/components/SectionHero/sectionHero";
 import { MDXRemote } from "next-mdx-remote";
 import { MDX_COMPONENTS } from "../../docs/common/mdx/constants";
+import {
+  Section,
+  SectionLayout,
+} from "../../components/content/content.styles";
+import { StyledSectionContent } from "../../components/Docs/components/Content/content.styles";
 
 export const LearnContentView = (props: StaticProps): JSX.Element => {
   const { frontmatter, mdxSource } = props;
@@ -13,7 +18,13 @@ export const LearnContentView = (props: StaticProps): JSX.Element => {
         head={frontmatter.title}
         subHead={frontmatter.subtitle}
       />
-      <MDXRemote {...mdxSource} components={MDX_COMPONENTS} />
+      <Section border>
+        <SectionLayout>
+          <StyledSectionContent>
+            <MDXRemote {...mdxSource} components={MDX_COMPONENTS} />
+          </StyledSectionContent>
+        </SectionLayout>
+      </Section>
     </Fragment>
   );
 };
