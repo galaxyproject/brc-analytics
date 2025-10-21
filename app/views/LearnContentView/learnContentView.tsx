546f3f81b5fcc68cf5440dfb9efc691c1e6f5b56
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-21 20:38:09 +1000

    feat: build static props (#331)

--- app/views/LearnContentView/learnContentView.tsx
+++ app/views/LearnContentView/learnContentView.tsx
@@ -1,9 +1,10 @@
 import { Fragment } from "react";
+import { StaticProps } from "../../docs/common/staticGeneration/types";
 
-export const LearnContentView = (): JSX.Element => {
+export const LearnContentView = (props: StaticProps): JSX.Element => {
   return (
     <Fragment>
-      <h1>Learn Content View</h1>
+      <h1>{props.pageTitle}</h1>
     </Fragment>
   );
 };
