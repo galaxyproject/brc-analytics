a8854991ac543f06631a2f631496d34740a59db1
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-21 20:44:37 +1000

    feat: added section hero to page (#331)

--- app/views/LearnContentView/learnContentView.tsx
+++ app/views/LearnContentView/learnContentView.tsx
@@ -1,10 +1,16 @@
 import { Fragment } from "react";
 import { StaticProps } from "../../docs/common/staticGeneration/types";
+import { SectionHero } from "../../components/Layout/components/AppLayout/components/Section/components/SectionHero/sectionHero";
 
 export const LearnContentView = (props: StaticProps): JSX.Element => {
   return (
     <Fragment>
-      <h1>{props.pageTitle}</h1>
+      <SectionHero
+        breadcrumbs={props.frontmatter.breadcrumbs || []}
+        head={props.frontmatter.title}
+        subHead={props.frontmatter.subtitle}
+      />
+      <h1>{props.frontmatter.description}</h1>
     </Fragment>
   );
 };
