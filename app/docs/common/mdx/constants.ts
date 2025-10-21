c099185736850454c9bbcce07dd4ea59868c4bd2
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-21 21:02:56 +1000

    feat: render page with mdx remote (#331)

---
+++ app/docs/common/mdx/constants.ts
@@ -0,0 +1,6 @@
+import { MDXComponents } from "mdx/types";
+import { AnchorLink } from "@databiosphere/findable-ui/lib/components/common/AnchorLink/anchorLink";
+
+export const MDX_COMPONENTS: MDXComponents = {
+  AnchorLink,
+};
