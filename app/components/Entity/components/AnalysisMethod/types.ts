95f23510519cc1fe2628902d8a3e44ed8935dbaf
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-10-29 12:06:06 +1000

    feat: move custom workflow under the accordion like other workflows (#934)

--- app/components/Entity/components/AnalysisMethod/types.ts
+++ app/components/Entity/components/AnalysisMethod/types.ts
@@ -1,9 +1,11 @@
+import { AccordionProps } from "@mui/material";
 import {
   Workflow,
   WorkflowCategory,
 } from "../../../../apis/catalog/brc-analytics-catalog/common/entities";
 
-export interface Props {
+export interface Props
+  extends Pick<AccordionProps, "defaultExpanded" | "disabled"> {
   entityId: string;
   geneModelUrl: string | null;
   genomeVersionAssemblyId: string;
--- app/components/Entity/components/AnalysisMethodsCatalog/analysisMethodsCatalog.tsx
+++ app/components/Entity/components/AnalysisMethodsCatalog/analysisMethodsCatalog.tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/router";
 import { Fragment } from "react";
 import { useFeatureFlag } from "@databiosphere/findable-ui/lib/hooks/useFeatureFlag/useFeatureFlag";
 import { CustomWorkflow } from "../AnalysisMethod/components/CustomWorkflow/customWorkflow";
+import { AnalysisTypeHeader } from "./components/AnalysisTypeHeader/analysisTypeHeader";
+import { Stack } from "@mui/material";
 
 export const AnalysisMethodsCatalog = ({ assembly }: Props): JSX.Element => {
   const isFeatureEnabled = useFeatureFlag("custom-workflow");
@@ -15,29 +17,62 @@ export const AnalysisMethodsCatalog = ({ assembly }: Props): JSX.Element => {
   } = useRouter();
 
   return (
-    <Fragment>
-      {isFeatureEnabled && <CustomWorkflow entityId={entityId as string} />}
-      {workflows.map((workflowCategory) => {
-        const compatibleWorkflows = workflowCategory.workflows.filter(
-          (workflow) => workflowIsCompatibleWithAssembly(workflow, assembly)
-        );
-        if (
-          compatibleWorkflows.length === 0 &&
-          !workflowCategory.showComingSoon
-        ) {
-          return null;
-        }
-        return (
-          <AnalysisMethod
-            entityId={entityId as string}
-            geneModelUrl={assembly.geneModelUrl}
-            genomeVersionAssemblyId={assembly.accession}
-            key={workflowCategory.category}
-            workflows={compatibleWorkflows}
-            workflowCategory={workflowCategory}
+    <Stack gap={8} useFlexGap>
+      {/* Custom Analysis */}
+      {isFeatureEnabled && (
+        <Stack gap={4} useFlexGap>
+          <AnalysisTypeHeader
+            description="Prefer to explore the data without a predefined workflow?"
+            title="Custom Analysis"
           />
-        );
-      })}
-    </Fragment>
+          <CustomWorkflow entityId={entityId as string} />
+        </Stack>
+      )}
+      <Stack gap={4} useFlexGap>
+        {/* Workflow Analysis */}
+        <AnalysisTypeHeader
+          description={
+            <Fragment>
+              <div>
+                Select an analysis workflow to run in Galaxy using this assembly
+                as your reference.
+              </div>
+              <div>
+                You can include sequencing reads from ENA and genome tracks from
+                the UCSC Genome Browser.
+              </div>
+              <div>
+                When you launch the analysis, a new Galaxy history will be
+                created containing the selected assembly and data.
+              </div>
+            </Fragment>
+          }
+          title="Select a Workflow"
+        />
+        {workflows.map((workflowCategory, i) => {
+          const compatibleWorkflows = workflowCategory.workflows.filter(
+            (workflow) => workflowIsCompatibleWithAssembly(workflow, assembly)
+          );
+          if (
+            compatibleWorkflows.length === 0 &&
+            !workflowCategory.showComingSoon
+          ) {
+            return null;
+          }
+          return (
+            <AnalysisMethod
+              defaultExpanded={i === 0}
+              disabled={compatibleWorkflows.length === 0}
+              entityId={entityId as string}
+              geneModelUrl={assembly.geneModelUrl}
+              genomeVersionAssemblyId={assembly.accession}
+              key={workflowCategory.category}
+              workflows={compatibleWorkflows}
+              workflowCategory={workflowCategory}
+            />
+          );
+        })}
+      </Stack>
+    </Stack>
   );
 };
