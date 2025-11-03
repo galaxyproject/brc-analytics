55e7f98aa3bd68aa267680832879eb1d020ac0be
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-11-03 14:29:54 +1000

    feat: map select category value for rendering track group (#920)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/RelatedTracksStep/components/GenomeBrowser/components/TracksSelector/hooks/UseTable/categoryConfigs.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/RelatedTracksStep/components/GenomeBrowser/components/TracksSelector/hooks/UseTable/categoryConfigs.ts
@@ -1,6 +1,14 @@
 import { CategoryConfig } from "@databiosphere/findable-ui/lib/common/categories/config/types";
+import { mapSelectCategoryValue } from "../../../../../../../../../../../../../../../../../../site-config/brc-analytics/local/index/common/category/mapSelectCategoryValue";
+import { GROUP_ID_LABEL } from "../../components/TracksSelectionPanel/constants";
 
 export const CATEGORY_CONFIGS: Record<string, CategoryConfig> = {
-  GROUP_ID: { key: "groupId", label: "Group ID" },
+  GROUP_ID: {
+    key: "groupId",
+    label: "Category",
+    mapSelectCategoryValue: mapSelectCategoryValue(
+      (label) => GROUP_ID_LABEL[label] || label
+    ),
+  },
   LONG_LABEL: { key: "longLabel", label: "Track Type" },
 } as const;
