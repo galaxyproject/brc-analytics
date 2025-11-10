d1dd5724a1dbf32848eed8a5d021d936e0ecd1de
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-11-10 12:02:56 +1000

    feat: allow display and selection of non-required sequencing step (#947)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/steps/utils.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/steps/utils.ts
@@ -9,20 +9,15 @@ import {
 import { CUSTOM_WORKFLOW } from "../../../../../../../../../components/Entity/components/AnalysisMethod/components/CustomWorkflow/constants";
 
 /**
- * Augment the configured steps with two additional sequencing steps "READ_RUNS_PAIRED" and "READ_RUNS_SINGLE"
- * if "READ_RUNS_ANY" is configured.
- * Allows the user to configure both single and paired end reads in the same step and render those values
- * in the summary.
+ * Augment the configured steps with two additional sequencing steps "READ_RUNS_PAIRED" and "READ_RUNS_SINGLE".
+ * Allows the user to configure both single and paired end reads and render those values in the summary.
  * @param configuredSteps - The configured steps.
  * @returns The augmented steps.
  */
 export function augmentConfiguredSteps(
   configuredSteps: StepConfig[]
 ): StepConfig[] {
-  if (configuredSteps.some((step) => step.key === "readRunsAny")) {
-    return [...configuredSteps, PAIRED_END_STEP, SINGLE_END_STEP];
-  }
-  return configuredSteps;
+  return [...configuredSteps, PAIRED_END_STEP, SINGLE_END_STEP];
 }
 
 /**
