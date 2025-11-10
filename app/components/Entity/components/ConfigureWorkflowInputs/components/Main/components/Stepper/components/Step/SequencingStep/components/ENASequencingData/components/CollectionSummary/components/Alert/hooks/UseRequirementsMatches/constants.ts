c76c5f968726991f4b392eab8d076b763a33b7e6
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-11-10 18:03:22 +1000

    feat: updated library strategy message (#947)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSummary/components/Alert/hooks/UseRequirementsMatches/constants.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SequencingStep/components/ENASequencingData/components/CollectionSummary/components/Alert/hooks/UseRequirementsMatches/constants.ts
@@ -5,5 +5,5 @@ export const COLUMN_KEY_TO_LABEL: Partial<Record<keyof ReadRun, string>> = {
   [CATEGORY_CONFIGS.DESCRIPTION.key]: "Description",
   [CATEGORY_CONFIGS.LIBRARY_LAYOUT.key]: "Library layout",
   [CATEGORY_CONFIGS.LIBRARY_SOURCE.key]: "Library source",
-  [CATEGORY_CONFIGS.LIBRARY_STRATEGY.key]: "Data type",
+  [CATEGORY_CONFIGS.LIBRARY_STRATEGY.key]: "Library strategy",
 };
