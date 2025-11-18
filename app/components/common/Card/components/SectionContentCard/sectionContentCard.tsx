7ab0af0a4e57a65fa57482a886c6918f3d9fb081
Fran McDade <18710366+frano-m@users.noreply.github.com>
2025-11-18 22:51:01 +1000

    feat: update learn section landing page (#985)

---
+++ app/components/common/Card/components/SectionContentCard/sectionContentCard.tsx
@@ -0,0 +1,41 @@
+import { CardTitle } from "@databiosphere/findable-ui/lib/components/common/Card/components/CardTitle/cardTitle";
+import { BaseComponentProps } from "@databiosphere/findable-ui/lib/components/types";
+import { CardActionArea } from "@databiosphere/findable-ui/lib/components/common/Card/components/CardActionArea/cardActionArea";
+import { CardProps } from "@databiosphere/findable-ui/lib/components/common/Card/card";
+import { ForwardArrowIcon } from "@databiosphere/findable-ui/lib/components/common/CustomIcon/components/ForwardArrowIcon/forwardArrowIcon";
+import { StyledStack, StyledCard } from "./sectionContentCard.styles";
+import { Typography } from "@mui/material";
+import { Props } from "./types";
+import {
+  CARD_PROPS,
+  STACK_PROPS,
+  SVG_ICON_PROPS,
+  TYPOGRAPHY_PROPS,
+} from "./constants";
+import { StaticImage } from "@databiosphere/findable-ui/lib/components/common/StaticImage/staticImage";
+
+export const SectionContentCard = ({
+  cardUrl,
+  className,
+  EndIcon = ForwardArrowIcon,
+  image,
+  secondaryText,
+  StartIcon,
+  title,
+}: BaseComponentProps &
+  Pick<CardProps, "cardUrl" | "secondaryText" | "title"> &
+  Props): JSX.Element => {
+  return (
+    <StyledCard {...CARD_PROPS} className={className}>
+      <CardActionArea cardUrl={cardUrl}>
+        {StartIcon && <StartIcon sx={{ fontSize: 48 }} />}
+        {image && <StaticImage {...image} />}
+        <EndIcon {...SVG_ICON_PROPS} />
+        <StyledStack {...STACK_PROPS}>
+          <CardTitle component="span">{title}</CardTitle>
+          <Typography {...TYPOGRAPHY_PROPS}>{secondaryText}</Typography>
+        </StyledStack>
+      </CardActionArea>
+    </StyledCard>
+  );
+};
---
+++ pages/learn/index.tsx
@@ -0,0 +1,18 @@
+import { LearnView } from "../../app/views/LearnView/learnView";
+import { GetStaticProps } from "next";
+import { StyledPagesMain } from "../../app/components/Layout/components/Main/main.styles";
+import { StaticProps } from "../../app/docs/common/staticGeneration/types";
+
+const Page = (): JSX.Element => {
+  return <LearnView />;
+};
+
+export const getStaticProps: GetStaticProps<
+  Pick<StaticProps, "pageTitle">
+> = async () => {
+  return { props: { pageTitle: "Learn About BRC" } };
+};
+
+export default Page;
+
+Page.Main = StyledPagesMain;
