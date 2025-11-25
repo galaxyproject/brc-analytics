136f650b3e5c223c373532fc7629f6b5cb7b3a73
Fran McDade <frano-m@users.noreply.github.com>
2025-11-25 13:29:29 +1000

    content: remove ai codeathon notice from the home carousel (#1019) (#1022)

--- app/components/Home/components/Section/components/SectionHero/components/Carousel/content/index.tsx
+++ app/components/Home/components/Section/components/SectionHero/components/Carousel/content/index.tsx
@@ -2,6 +2,5 @@ export { default as ShareUsageAndJoinAdvisoryPanel } from "./shareUsageAndJoinAd
 export { default as Webinar20250326 } from "./webinar20250326.mdx";
 export { default as Webinars } from "./webinars.mdx";
 export { default as Roadmap } from "./roadmap.mdx";
-export { default as Codeathon2025 } from "./codeathon2025.mdx";
 export { default as WebinarSeries } from "./webinarSeries.mdx";
 export { default as EvolutionaryDynamicsOfCodingOverlapsInMeaslesVirus } from "./evolutionary-dynamics-of-coding-overlaps-in-measles-virus.mdx";
