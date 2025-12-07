2b4173b20fc6ec92626b351ef3efa421da75f418
Danielle Callan <dmleake92@gmail.com>
2025-12-07 17:27:08 -0500

    fix: generalize error for when plausible config isnt set for prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,10 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 const siteConfig = process.env.NEXT_PUBLIC_SITE_CONFIG;
 const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
 const isProd = Boolean(siteConfig && siteConfig.endsWith("-prod"));
-const requiresPlausible = siteConfig === "brc-analytics-prod";
 
-if (requiresPlausible && !plausibleDomain) {
+if (isProd && !plausibleDomain) {
   throw new Error(
-    "NEXT_PUBLIC_PLAUSIBLE_DOMAIN is not defined for brc-analytics production"
+    `NEXT_PUBLIC_PLAUSIBLE_DOMAIN is not defined in production environment for ${siteConfig}`
   );
 }
 
