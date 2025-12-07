7152fb58eb16b833a458f8806bf35780c8bf08bf
Danielle Callan <dmleake92@gmail.com>
2025-12-07 17:16:23 -0500

    fix: plausible site config optional and only set for prod sites

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,20 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+const siteConfig = process.env.NEXT_PUBLIC_SITE_CONFIG;
 const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
+const isProd = Boolean(siteConfig && siteConfig.endsWith("-prod"));
+const requiresPlausible = siteConfig === "brc-analytics-prod";
 
-if (!plausibleDomain) {
-  throw new Error("NEXT_PUBLIC_PLAUSIBLE_DOMAIN is not defined");
+if (requiresPlausible && !plausibleDomain) {
+  throw new Error(
+    "NEXT_PUBLIC_PLAUSIBLE_DOMAIN is not defined for brc-analytics production"
+  );
+}
+
+if (!isProd && plausibleDomain) {
+  console.warn(
+    `Plausible is enabled (domain='${plausibleDomain}') while NEXT_PUBLIC_SITE_CONFIG='${siteConfig}'.`
+  );
 }
 
 class MyDocument extends Document {
@@ -19,11 +30,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@500&display=swap"
             rel="stylesheet"
           />
-          <script
-            data-domain={plausibleDomain}
-            defer
-            src="https://plausible.galaxyproject.eu/js/script.js"
-          />
+          {plausibleDomain && (
+            <script
+              data-domain={plausibleDomain}
+              defer
+              src="https://plausible.galaxyproject.eu/js/script.js"
+            />
+          )}
         </Head>
         <body>
           <Main />
