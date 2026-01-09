131da45a97635a897c5ebbbbdb311176792a3b54
Fran McDade <frano-m@users.noreply.github.com>
2026-01-09 14:35:46 +1000

    feat: update learn section landing page (#985) (#1005)

---
+++ app/components/common/Card/components/SectionContentCard/sectionContentCard.styles.ts
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import { Card, Stack } from "@mui/material";
+
+export const StyledCard = styled(Card)`
+  &.MuiPaper-root {
+    align-items: stretch;
+    display: flex;
+  }
+
+  .MuiButtonBase-root {
+    align-content: flex-start;
+    display: grid;
+    gap: 16px;
+    grid-template-columns: 1fr;
+    padding: 16px;
+
+    .MuiSvgIcon-root {
+      box-sizing: content-box;
+    }
+
+    img {
+      margin: 0;
+    }
+  }
+` as typeof Card;
+
+export const StyledStack = styled(Stack)`
+  grid-column: 1 / -1;
+`;
