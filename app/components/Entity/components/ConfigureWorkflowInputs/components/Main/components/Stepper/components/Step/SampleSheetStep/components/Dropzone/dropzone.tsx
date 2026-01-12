a59f121be5e2b179bca399d919877cb83355878b
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-01-12 15:05:01 +1000

    fix: dropzone with keyboard accessibility (#1060)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SampleSheetStep/components/Dropzone/dropzone.tsx
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SampleSheetStep/components/Dropzone/dropzone.tsx
@@ -1,5 +1,5 @@
 import { Stack, Typography } from "@mui/material";
-import { StyledPaper } from "./dropzone.styles";
+import { StyledButtonBase, StyledPaper } from "./dropzone.styles";
 import { TYPOGRAPHY_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/typography";
 import { Dot } from "@databiosphere/findable-ui/lib/components/common/Dot/dot";
 import { FileUploadIcon } from "../../../../../../../../../../../../common/CustomIcon/components/FileUploadIcon/fileUploadIcon";
@@ -8,26 +8,28 @@ import { STACK_PROPS } from "./constants";
 
 export const Dropzone = ({ onClick }: Props): JSX.Element => {
   return (
-    <StyledPaper elevation={0} onClick={onClick}>
-      <FileUploadIcon />
-      <Stack {...STACK_PROPS}>
-        <Typography
-          component="div"
-          variant={TYPOGRAPHY_PROPS.VARIANT.HEADING_XSMALL}
-        >
-          Upload a sample sheet
-        </Typography>
-        <Typography
-          color={TYPOGRAPHY_PROPS.COLOR.INK_LIGHT}
-          variant={TYPOGRAPHY_PROPS.VARIANT.BODY_400}
-        >
-          <Stack direction="row" gap={1} useFlexGap>
-            <span>Click to browse files</span>
-            <Dot />
-            <span>CSV, TSV supported</span>
-          </Stack>
-        </Typography>
-      </Stack>
+    <StyledPaper elevation={0}>
+      <StyledButtonBase aria-label="Upload a sample sheet" onClick={onClick}>
+        <FileUploadIcon />
+        <Stack {...STACK_PROPS}>
+          <Typography
+            component="div"
+            variant={TYPOGRAPHY_PROPS.VARIANT.HEADING_XSMALL}
+          >
+            Upload a sample sheet
+          </Typography>
+          <Typography
+            color={TYPOGRAPHY_PROPS.COLOR.INK_LIGHT}
+            variant={TYPOGRAPHY_PROPS.VARIANT.BODY_400}
+          >
+            <Stack direction="row" gap={1} useFlexGap>
+              <span>Click to browse files</span>
+              <Dot />
+              <span>CSV, TSV supported</span>
+            </Stack>
+          </Typography>
+        </Stack>
+      </StyledButtonBase>
     </StyledPaper>
   );
 };
