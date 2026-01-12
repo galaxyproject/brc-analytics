5928edbdbd8308f9edc9a9eac7e76e954eb6f772
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-01-12 14:52:19 +1000

    fix: aria-label (#1060)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SampleSheetStep/constants.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/SampleSheetStep/constants.ts
@@ -4,6 +4,7 @@ export const ACCEPTED_FILE_TYPES = ".csv,.tsv";
 
 export const INPUT_PROPS: ComponentProps<"input"> = {
   accept: ACCEPTED_FILE_TYPES,
+  "aria-label": "Upload sample sheet file",
   hidden: true,
   type: "file",
 };
