8e35ee34de0eb60247ddcda664cf3fa594d5e1ec
mvdbeek <m.vandenbeek@gmail.com>
2026-01-18 16:09:21 +0100

    WIP: wire up DE workflow in stepper

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/hooks/UseLaunchGalaxy/types.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/hooks/UseLaunchGalaxy/types.ts
@@ -2,12 +2,16 @@ import { EnaSequencingReads } from "app/utils/galaxy-api/entities";
 import { Workflow } from "../../../../../../../../../../../../apis/catalog/brc-analytics-catalog/common/entities";
 import { ConfiguredInput } from "../../../../../../../../../../../../views/WorkflowInputsView/hooks/UseConfigureInputs/types";
 import { UcscTrack } from "../../../../../../../../../../../../utils/ucsc-tracks-api/entities";
+import { COLUMN_TYPE } from "../../SampleSheetClassificationStep/types";
 
 export interface ConfiguredValue {
+  designFormula: string | null;
   geneModelUrl: string | null;
   readRunsPaired: EnaSequencingReads[] | null;
   readRunsSingle: EnaSequencingReads[] | null;
   referenceAssembly: string;
+  sampleSheet: Record<string, string>[] | null;
+  sampleSheetClassification: Record<string, COLUMN_TYPE | null> | null;
   tracks: UcscTrack[] | null;
 }
 
