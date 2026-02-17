99acbb2a730b2ddb3c9280f097469f3765cd4ec4
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-02-17 22:28:12 +1000

    feat: add workflows list page and explore view components (#1145)

---
+++ app/views/WorkflowsView/components/Workflows/workflows.tsx
@@ -0,0 +1,30 @@
+import { FilterButton } from "@databiosphere/findable-ui/lib/components/Index/components/EntityView/components/controls/FilterButton/filterButton";
+import { Title } from "@databiosphere/findable-ui/lib/components/Index/components/EntityView/components/layout/Title/title";
+import { useLayoutSpacing } from "@databiosphere/findable-ui/lib/hooks/UseLayoutSpacing/hook";
+import React, { JSX } from "react";
+import { Card } from "./components/Card/card";
+import { Props } from "./types";
+import { StyledContainer, StyledStack } from "./workflows.styles";
+
+/**
+ * Component rendering the workflow grid view.
+ * @param props - Props.
+ * @param props.table - Table.
+ * @returns Workflows grid.
+ */
+export const Workflows = ({ table }: Props): JSX.Element => {
+  const { spacing } = useLayoutSpacing();
+  const { getRowModel } = table;
+  const { rows } = getRowModel();
+  return (
+    <StyledContainer maxWidth={false} {...spacing}>
+      <StyledStack useFlexGap>
+        <Title />
+        <FilterButton />
+      </StyledStack>
+      {rows.map((row) => (
+        <Card key={row.id} row={row} />
+      ))}
+    </StyledContainer>
+  );
+};
