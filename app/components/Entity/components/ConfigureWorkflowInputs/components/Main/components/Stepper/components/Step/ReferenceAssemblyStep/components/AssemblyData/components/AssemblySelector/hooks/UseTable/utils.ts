50c94ef0d7dab58fed34f2567c182374125d5e6f
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-03-03 16:23:29 +1000

    feat: update init columnfilters with taxonomyid (#1147)

--- app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/ReferenceAssemblyStep/components/AssemblyData/components/AssemblySelector/hooks/UseTable/utils.ts
+++ app/components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/ReferenceAssemblyStep/components/AssemblyData/components/AssemblySelector/hooks/UseTable/utils.ts
@@ -15,15 +15,32 @@ export function enableRowSelection(row: Row<Assembly>): boolean {
 
 /** Returns the initial column filters for the table based on the workflow's ploidy.
  * If the workflow's ploidy is not "ANY", it pre-selects the ploidy filter with the workflow's ploidy value.
+ * If the workflow has a taxonomy requirement, it pre-selects the taxonomy filter with the workflow's taxonomy ID.
  * @param workflow - Workflow to get the ploidy from.
  * @returns The initial column filters for the table.
  */
 export function getInitialColumnFilters(
   workflow: Workflow
 ): ColumnFiltersState {
-  if (workflow.ploidy === WORKFLOW_PLOIDY.ANY) return [];
+  const columnFilterState = [];
 
-  return [{ id: CATEGORY_CONFIGS.PLOIDY.key, value: [workflow.ploidy] }];
+  // If the workflow has a specific ploidy requirement, pre-select that ploidy in the filters.
+  if (workflow.ploidy !== WORKFLOW_PLOIDY.ANY) {
+    columnFilterState.push({
+      id: CATEGORY_CONFIGS.PLOIDY.key,
+      value: [workflow.ploidy],
+    });
+  }
+
+  // If the workflow has a specific taxonomy requirement, pre-select that taxonomy in the filters.
+  if (workflow.taxonomyId) {
+    columnFilterState.push({
+      id: CATEGORY_CONFIGS.TAXONOMY_ID.key,
+      value: [workflow.taxonomyId],
+    });
+  }
+
+  return columnFilterState;
 }
 
 /**
