7e7340d071049a799348032d65a0ea060f2af169
Danielle Callan <dmleake92@gmail.com>
2026-03-06 11:57:11 -0500

    feat: enable ga2 roadmap w updated content

--- app/views/RoadmapView/roadmapViewGA2.tsx
+++ app/views/RoadmapView/roadmapViewGA2.tsx
@@ -3,8 +3,6 @@ import { Fragment } from "react";
 import { SectionRoadmapGA2 } from "../../components/content";
 import { SectionHero } from "../../components/Layout/components/AppLayout/components/Section/components/SectionHero/sectionHero";
 import { BREADCRUMBS } from "./common/constants";
-import { Typography } from "@mui/material";
-import { TYPOGRAPHY_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/typography";
 
 export const RoadmapViewGA2 = (): JSX.Element => {
   return (
@@ -15,16 +13,12 @@ export const RoadmapViewGA2 = (): JSX.Element => {
         subHead={
           <>
             GenomeArk lets you combine VGP Phase 1, official NCBI, and your own
-            data using Galaxy Platform. It simplifies large-scale
-            bioinformatics, ensures reproducibility, and makes results easy to
-            share with other researchers. Our goal is to create a{" "}
-            <Typography
-              component="span"
-              variant={TYPOGRAPHY_PROPS.VARIANT.BODY_LARGE_500}
-            >
-              community-driven knowledge platform
-            </Typography>{" "}
-            that lowers barriers to entry for complex bioinformatics analyses.
+            data using the Galaxy Platform. We are extending the reach of the
+            VGP project by making it simple to analyze those assemblies,
+            generate new ones with proven workflows, and feed results back to
+            the community. By emphasizing reproducibility, collaboration, and
+            low-barrier access to large-scale bioinformatics, we help
+            researchers turn high-quality genomes into actionable science.
           </>
         }
       />
