0613f73e1a10dc9c1513a4d148a7cbe939789bdb
Danielle Callan <53306535+d-callan@users.noreply.github.com>
2026-03-06 14:56:44 -0500

    feat: enable ga2 roadmap w updated content (#1173)

--- site-config/ga2/local/config.ts
+++ site-config/ga2/local/config.ts
@@ -20,7 +20,7 @@ const ALLOWED_PATHS = [
   ROUTES.ABOUT,
   ROUTES.ORGANISMS,
   ROUTES.GENOMES,
-  // ROUTES.ROADMAP,
+  ROUTES.ROADMAP,
 ];
 const LOCALHOST = "http://localhost:3000";
 const APP_TITLE = "Genome Ark 2";
@@ -88,7 +88,7 @@ export function makeConfig(
             { label: "About", url: ROUTES.ABOUT },
             { label: "Organisms", url: ROUTES.ORGANISMS },
             { label: "Assemblies", url: ROUTES.GENOMES },
-            // { label: "Roadmap", url: ROUTES.ROADMAP },
+            { label: "Roadmap", url: ROUTES.ROADMAP },
           ],
           undefined,
         ],
