ec42304d50b5595a67f6f48b18e7b5fcaa89c1c2
Fran McDade <frano-m@users.noreply.github.com>
2026-03-10 14:06:05 +1000

    feat: sidebar assembly details (#1149) (#1176)
    Co-authored-by: Fran McDade <18710366+frano-m@users.noreply.github.com>

--- app/viewModelBuilders/catalog/brc-analytics-catalog/common/viewModelBuilders.ts
+++ app/viewModelBuilders/catalog/brc-analytics-catalog/common/viewModelBuilders.ts
@@ -3,7 +3,18 @@ import {
   Key,
   Value,
 } from "@databiosphere/findable-ui/lib/components/common/KeyValuePairs/keyValuePairs";
+import { FluidPaper } from "@databiosphere/findable-ui/lib/components/common/Paper/paper.styles";
+import { COLUMN_IDENTIFIER } from "@databiosphere/findable-ui/lib/components/Table/common/columnIdentifier";
+import { CHIP_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/chip";
+import { ColumnDef, getSortedRowModel } from "@tanstack/react-table";
+import { LinkProps } from "next/link";
+import Router from "next/router";
 import { ComponentProps } from "react";
+import {
+  BRC_DATA_CATALOG_CATEGORY_KEY,
+  BRC_DATA_CATALOG_CATEGORY_LABEL,
+} from "site-config/brc-analytics/category";
+import slugify from "slugify";
 import { ROUTES } from "../../../../../routes/constants";
 import {
   BRCDataCatalogGenome,
@@ -11,7 +22,25 @@ import {
   Outbreak,
   Workflow,
 } from "../../../../apis/catalog/brc-analytics-catalog/common/entities";
+import {
+  getGenomeOrganismId,
+  getOrganismId,
+} from "../../../../apis/catalog/brc-analytics-catalog/common/utils";
+import { sanitizeEntityId } from "../../../../apis/catalog/common/utils";
+import {
+  GA2AssemblyEntity,
+  GA2OrganismEntity,
+} from "../../../../apis/catalog/ga2/entities";
+import { SLUGIFY_OPTIONS } from "../../../../common/constants";
 import * as C from "../../../../components";
+import { StepConfig } from "../../../../components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/types";
+import { KeyValueSection } from "../../../../components/Entity/components/Section/KeyValueSection/keyValueSection";
+import {
+  getPriorityColor,
+  getPriorityLabel,
+} from "../../../../views/PriorityPathogensView/components/PriorityPathogens/utils";
+import { ResourcesSection } from "../../../../views/PriorityPathogenView/components/ResourcesSection/resourcesSection";
+import { ConfiguredInput } from "../../../../views/WorkflowInputsView/hooks/UseConfigureInputs/types";
 import {
   GALAXY_DATACACHE,
   GENOME_BROWSER,
@@ -19,35 +48,6 @@ import {
   NCBI_DATASETS_URL,
   NCBI_TAXONOMY,
 } from "./constants";
-import { ColumnDef, getSortedRowModel } from "@tanstack/react-table";
-import {
-  BRC_DATA_CATALOG_CATEGORY_KEY,
-  BRC_DATA_CATALOG_CATEGORY_LABEL,
-} from "site-config/brc-analytics/category";
-import {
-  getGenomeOrganismId,
-  getOrganismId,
-} from "../../../../apis/catalog/brc-analytics-catalog/common/utils";
-import { COLUMN_IDENTIFIER } from "@databiosphere/findable-ui/lib/components/Table/common/columnIdentifier";
-import { ConfiguredInput } from "../../../../views/WorkflowInputsView/hooks/UseConfigureInputs/types";
-import { CHIP_PROPS } from "@databiosphere/findable-ui/lib/styles/common/mui/chip";
-import {
-  getPriorityColor,
-  getPriorityLabel,
-} from "../../../../views/PriorityPathogensView/components/PriorityPathogens/utils";
-import { KeyValueSection } from "../../../../components/Entity/components/Section/KeyValueSection/keyValueSection";
-import { ResourcesSection } from "../../../../views/PriorityPathogenView/components/ResourcesSection/resourcesSection";
-import Router from "next/router";
-import slugify from "slugify";
-import { SLUGIFY_OPTIONS } from "../../../../common/constants";
-import { LinkProps } from "next/link";
-import { FluidPaper } from "@databiosphere/findable-ui/lib/components/common/Paper/paper.styles";
-import {
-  GA2AssemblyEntity,
-  GA2OrganismEntity,
-} from "../../../../apis/catalog/ga2/entities";
-import { sanitizeEntityId } from "../../../../apis/catalog/common/utils";
-import { StepConfig } from "../../../../components/Entity/components/ConfigureWorkflowInputs/components/Main/components/Stepper/components/Step/types";
 
 /**
  * Build props for the accession cell.
@@ -953,6 +953,10 @@ export const buildWorkflowConfiguration = (
     const value = stepConfig.renderValue?.(configuredInput);
     if (value === undefined) continue;
     keyValuePairs.set(stepConfig.label, value);
+  }
+  // If there are no configured inputs, add a "None" value.
+  if (keyValuePairs.size === 0) {
+    keyValuePairs.set("", "None");
   }
   return {
     KeyElType: C.KeyElType,
--- app/views/WorkflowView/components/SideColumn/types.ts
+++
@@ -1,5 +0,0 @@
-import { Workflow } from "../../../../apis/catalog/brc-analytics-catalog/common/entities";
-
-export interface Props {
-  workflow: Workflow;
-}
