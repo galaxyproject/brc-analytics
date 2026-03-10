7d493a7b55268a5cc48a85b0e8f207548fbc5311
Fran McDade <18710366+frano-m@users.noreply.github.com>
2026-03-10 16:49:07 +1000

    refactor: import types (#1144)

--- app/views/WorkflowsView/components/Workflows/components/Card/types.ts
+++ app/views/WorkflowsView/components/Workflows/components/Card/types.ts
@@ -1,5 +1,5 @@
 import { Row } from "@tanstack/react-table";
-import { WorkflowEntity } from "../../../../../../../site-config/brc-analytics/local/index/workflow/types";
+import type { WorkflowEntity } from "../../../../../../../site-config/brc-analytics/local/index/workflow/types";
 
 export interface Props {
   row: Row<WorkflowEntity>;
--- app/views/WorkflowsView/components/Workflows/types.ts
+++ app/views/WorkflowsView/components/Workflows/types.ts
@@ -1,5 +1,5 @@
 import { Table } from "@tanstack/react-table";
-import { WorkflowEntity } from "../../../../../site-config/brc-analytics/local/index/workflow/types";
+import type { WorkflowEntity } from "../../../../../site-config/brc-analytics/local/index/workflow/types";
 
 export interface Props {
   table: Table<WorkflowEntity>;
