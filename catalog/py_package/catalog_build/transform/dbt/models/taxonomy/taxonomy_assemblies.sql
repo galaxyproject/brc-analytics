d04d31f812609100bb05190b3df98d8cdfcf9107
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-07-05 23:25:05 -0700

    fix: list lineage tax ids in descending order (#1367)

--- catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_assemblies.sql
+++ catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_assemblies.sql
@@ -6,7 +6,7 @@ select
   first(l.taxon_name) filter (l.rank = '{{taxonomic_level}}') as taxonomic_level_{{taxonomic_level}},
   first(l.tax_id) filter (l.rank = '{{taxonomic_level}}') as taxonomic_level_{{taxonomic_level}}_id,
   {% endfor %}
-  list(l.tax_id order by l.depth) as lineage_taxonomy_ids,
+  list(l.tax_id order by l.depth desc) as lineage_taxonomy_ids,
   first(l.common_names) filter (l.is_query_taxon) as common_names
 from {{ source("catalog", "assembly_taxa") }} t
 join {{ ref("taxonomy_lineages_with_names") }} l on l.query_tax_id = t.taxonomy_id
