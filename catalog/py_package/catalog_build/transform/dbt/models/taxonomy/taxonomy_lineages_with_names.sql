85f286b54bd3f7b17b4e24505d014243226f36df
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-07-06 19:33:10 -0700

    feat: include other types of common names (#1367)

--- catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_lineages_with_names.sql
+++ catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_lineages_with_names.sql
@@ -3,7 +3,11 @@
 select
   l.*,
   first(n.name_txt) filter (n.name_class = 'scientific name') as taxon_name,
-  coalesce(list(n.name_txt) filter (n.name_class = 'common name'), []) as common_names
+  -- List different types of common names, ensuring that genbank common name comes first if present
+  list_concat(
+    list(n.name_txt) filter (n.name_class = 'genbank common name'),
+    list(n.name_txt) filter (n.name_class = 'common name' or n.name_class = 'acronym')
+  ) as common_names
 from {{ ref("taxonomy_lineages") }} l
 join {{ source("ncbi_taxonomy_raw", "ncbi_taxonomy_names") }} n on n.tax_id = l.tax_id
 group by all
