00ae4621ad0cd7b407d265e0110910958832e6a0
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-07-10 16:01:26 -0700

    feat: use left join for assembly taxonomy (#1367)

--- catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_assemblies.sql
+++ catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_assemblies.sql
@@ -6,8 +6,14 @@ select
   first(l.taxon_name) filter (l.rank = '{{taxonomic_level}}') as taxonomic_level_{{taxonomic_level}},
   first(l.tax_id) filter (l.rank = '{{taxonomic_level}}') as taxonomic_level_{{taxonomic_level}}_id,
   {% endfor %}
-  list(l.tax_id order by l.depth desc) as lineage_taxonomy_ids,
-  first(l.common_names) filter (l.is_query_taxon) as common_names
+  coalesce(
+    list(l.tax_id order by l.depth desc),
+    []
+  ) as lineage_taxonomy_ids,
+  coalesce(
+    first(l.common_names) filter (l.is_query_taxon),
+    []
+  ) as common_names
 from {{ source("catalog", "assembly_taxa") }} t
-join {{ ref("taxonomy_lineages_with_names") }} l on l.query_tax_id = t.taxonomy_id
+left join {{ ref("taxonomy_lineages_with_names") }} l on l.query_tax_id = t.taxonomy_id
 group by t.taxonomy_id
