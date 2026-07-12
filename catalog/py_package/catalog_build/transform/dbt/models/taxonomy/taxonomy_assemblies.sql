f3eada122968e2495b99354742226b4ee379277c
hunterckx <118154470+hunterckx@users.noreply.github.com>
2026-07-11 23:35:44 -0700

    refactor: remove coalesce calls unneeded after left join removal (#1367)

--- catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_assemblies.sql
+++ catalog/py_package/catalog_build/transform/dbt/models/taxonomy/taxonomy_assemblies.sql
@@ -6,14 +6,8 @@ select
   first(l.taxon_name) filter (l.rank = '{{taxonomic_level}}') as taxonomic_level_{{taxonomic_level}},
   first(l.tax_id) filter (l.rank = '{{taxonomic_level}}') as taxonomic_level_{{taxonomic_level}}_id,
   {% endfor %}
-  coalesce(
-    list(l.tax_id order by l.depth desc),
-    []
-  ) as lineage_taxonomy_ids,
-  coalesce(
-    first(l.common_names) filter (l.is_query_taxon),
-    []
-  ) as common_names
+  list(l.tax_id order by l.depth desc) as lineage_taxonomy_ids,
+  first(l.common_names) filter (l.is_query_taxon) as common_names
 from {{ source("catalog_source", "assembly_taxa") }} t
 join {{ ref("taxonomy_lineages_with_names") }} l on l.query_tax_id = t.taxonomy_id
 group by t.taxonomy_id
